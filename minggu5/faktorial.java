42f821a19b7385866198186b5f00e90715c47904
Callista Dinar Kushermyla <callistakushermyla731@gmail.com>
2026-03-29 19:04:50 +0700

    Modifikasi pertanyaan 5.2 No. 2

--- minggu5/faktorial.java
+++ minggu5/faktorial.java
@@ -3,8 +3,10 @@ package minggu5;
 public class faktorial {
     int faktorialBF(int n) {
         int fakto = 1;
-        for(int i = 1; i <= n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+        fakto = fakto * i;
+        i++;
         }
         return fakto;
     }
